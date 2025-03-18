a688445500fbef75ac2022b60c79ac2a394e361b
m2ri16 <00000845849.marina@unicap.br>
2025-03-17 22:04:53 -0300

    Acesso aos elementos da matriz

---
+++ acessoAosElementosDaMatriz.c
@@ -0,0 +1,5 @@
+int a[2][3] = {{0,4,5}, {-2,3,-7}};
+
+printf("a[0][1] = %d", a[0][1]); // 4 (linha 0 coluna 1)
+printf("a[1][2] = %d", a[1][2]); //-7 (linha 1 coluna 2)
+printf("a[2][3] = %d", a[2][3]); // valor inválido
