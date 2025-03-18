898dbd266166bad2e3b2f968dcae469f8dbb0f38
m2ri16 <00000845849.marina@unicap.br>
2025-03-17 22:07:36 -0300

    Imprimir os elementos da matriz

---
+++ imprimirOsElementosDaMatriz.c
@@ -0,0 +1,7 @@
+int a[10][3];
+...
+    for (i = 0; i < 10; i++) { // anda na linha
+        for (j = 0; j < 3; j++) { // anda na coluna
+            printf("a[%d][%d] = %d\n", i, j, a[i][j]);
+    }
+}
