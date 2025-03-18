c011166ae32c2d8803e0ddc12aca4fd71e888e33
m2ri16 <00000845849.marina@unicap.br>
2025-03-17 22:03:14 -0300

    Declaração de matriz

---
+++ declaracaoDeMatriz.c
@@ -0,0 +1,9 @@
+int A[10][3]; // matriz A com 10 linhas e 3 colunas
+char M[3][2]; // matriz M com 3 linhas e 2 colunas
+
+int A[2][5][10][3]; // matriz de quatro dimensões
+char M[6][3][2]; // matriz de três dimensões
+
+int A[2][3] = {{0,4,5}, {-2,3,-7}};
+char M[3][2] = {{’a’,’x’},{’b’,’h’},{’D’,’j’}};
+
